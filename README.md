3f36086ea0869993200a36693c37def267c2d98f
Benjamin-Re <renker.benjamin@gmail.com>
2022-06-12 11:49:33 +0200

    Add basic layout and navigation for webshop

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
+Webstore
+A website where a user can browser products, sign up, log in, add products to his cart, increase and decrease their quantity, and buy them.
+The signup is a form with the fields
+- shipping address
+- name
+- email
+- payment method
+- password
+
+The user has to be signed in to complete checkeout process.
+
+After a sucessful checkout an order confirmation should be displayed to the user. And possibly a confirmation email should be sent as well.
+
+The user should be able to change his details after signing up.
+
+There should also be an admin login, so the shop owner can add products without touching any code or the database directly.
+The admin could also keep track of customers and orders this way.
+
+
+
+### More Ideas 
 1. Consume an API
 https://mixedanalytics.com/blog/list-actually-free-open-no-auth-needed-apis/
 
--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import {Layout} from "./components/Layout";
-import {GeneralContext} from "./components/Model";
-import { createContext, useState, useContext } from "react";
-
-export const Context = createContext();
+import { Nav } from "./components/Nav";
+import { Routes, Route } from "react-router-dom";
+import { Products } from "./components/Products";
+import { Login } from "./components/Login";
+import { Cart } from "./components/Cart";
+import { Home } from "./components/Home";
 
 function App() {
-  const [letter, setLetter] = useState("B");
-  
   return (
     <>
-    <Context.Provider value={{letter, setLetter}}>
-      <Layout></Layout>
-    </Context.Provider>
-    </>  
+      <Nav></Nav>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </>
   );
-
-
 }
 
-
 export default App;
-
-/*
-- Get data from API
-- Store data in DB
-- Multiplayer with sockets
-*/
