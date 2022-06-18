f992360d76f55eb5cc64e635f5ab73239f3e044b
Benjamin-Re <renker.benjamin@gmail.com>
2022-06-18 19:55:58 +0200

    Add backend, config cors, frontend fetch to backend successful, add gitignore

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,8 +1,15 @@
+import React, { useEffect } from "react";
 import { Card } from "./Card";
 import css from './products.css';
 
 
 export function Products() {
+
+useEffect(()=> {
+  fetch(`http://localhost:8000/products`)
+  .then(res => res.json())
+  .then(data => {console.log(data)})
+}, [])
 
   return (
     <>
@@ -10,7 +17,6 @@ export function Products() {
       <div className="products-container">
         <Card id="1" name="chair" price="99€"></Card>
         <Card id="2" name="table" price="299€"></Card>
-       
       </div>
     </>
   );
